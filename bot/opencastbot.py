dcd0c9464b9416757581aa6924ae9fd735440500
Helio Loureiro <helio.loureiro@ericsson.com>
2018-10-05 21:32:18 +0200

    Fixed wrong placement.

--- bot/opencastbot.py
+++ bot/opencastbot.py
@@ -472,7 +472,7 @@ def PautaHandler(cmd):
         else:
             error("No commands found for: %s" % msg)
 
-    bot.reply_to(msg)
+        bot.reply_to(msg)
     except Exception as e:
         try:
             bot.reply_to(cmd, "Erro: %s" % e)
